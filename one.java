d8204946ed83cbb0079888b1109efb5b9fa63d6e
ManjuMuralidharan <manju1695@gmail.com>
2016-08-09 18:34:09 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 char ch;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter an Alphabet : ");
+        ch = scan.next().charAt(0);
+		
+        if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ||
+        ch=='i' || ch=='I' || ch=='o' || ch=='O' ||
+        ch=='u' || ch=='U')
+        {
+            System.out.print("This is a Vowel");
+        }
+        else
+        {
+            System.out.print("This is not a Vowel");
+        }
+
+
+
+			}
+
+}
